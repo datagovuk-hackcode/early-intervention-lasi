3d6a589cc85b28be5a6f71d44a2cad11782bcc52
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-03-25 11:31:33 +0000

    Some refactoring

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
-var csv = require('csv'),
+var censusReader = require('./censusReader'),
 	restify = require('restify'),
 	underscore = require('underscore');
 
-function getLastYearWorked (geography, callback) {
-	geography = geography.toLowerCase();
-	csv()
-		.from.path('data/nomisweb-data-year-last-worked.csv', {
-			columns: true
-		})
-		.to.array(function (data, count) {
-			callback(null, parseInt(data[0]['Year Last Worked: Last worked in 2011; measures: Value']));
-		})
-		.transform(function (row) {
-			if ((row["Rural Urban"] !== "Total") ||
-				(row["geography"].toLowerCase() !== geography)) return null;
-			return row;
-		})
-		.on('close', function(count){
-		  // when writing to a file, use the 'close' event
-		  // the 'end' event may fire before the file has been written
-		  console.log('Number of lines: '+count);
-		});
-}
-
 function getCensusParameter (req, res, next) {
 	var f = { 
-		'lastYearWorked': getLastYearWorked 
+		'lastYearWorked': censusReader.getLastYearWorked 
 	}[req.params.parameter];
 	f(req.params.geography, function (err, result) {
 		res.send({ result: result });
